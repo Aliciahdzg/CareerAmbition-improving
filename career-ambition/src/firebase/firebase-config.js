7a2b1ad3b26a6df110246e40d47d963c4534834f
Isabel <isvaoc@gmail.com>
2022-01-17 18:31:24 -0600

    creando el proyecto de react e instalando las paqueterias que usaremos

---
+++ career-ambition/src/firebase/firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaM64nNafIiUOiMVVMfed91uhV_lSHuRo",
+  authDomain: "improving-tf.firebaseapp.com",
+  projectId: "improving-tf",
+  storageBucket: "improving-tf.appspot.com",
+  messagingSenderId: "568383482690",
+  appId: "1:568383482690:web:14ddc343d583991d0e76db"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
