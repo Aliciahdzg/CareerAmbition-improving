de0f057f642c69d9148c6670bff350cd41babc2f
Alba <alba.lp95@gmail.com>
2022-01-19 17:56:25 -0600

    components and login

---
+++ career-ambition/src/components/Login/Login.js
@@ -0,0 +1,25 @@
+import './login.css'
+export default function Login() {
+
+    return(
+        <div>
+            <div>
+                <img src="https://i.ibb.co/7k4WzpX/improving-logo-color.png" alt="logo" />
+            </div>
+            <div className="formLogin">
+                <form >
+                <h3>Login</h3>
+                <p>Name:</p>
+                    <input type="text" />
+                <p>Email:</p>
+                    <input type="text" />
+                <p>Password:</p>
+                    <input type="text" />
+                <p><button type="submit">Get in</button> </p>
+            
+                </form>
+            </div>
+        </div>
+
+    )
+}
