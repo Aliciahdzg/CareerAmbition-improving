ad6eccf89a2af64e2ba9b8d4e247e85c72e4be49
fanita90 <parroquini_90@hotmail.com>
2022-01-20 10:41:18 -0600

    se agregó función de Login

--- career-ambition/src/components/Login/Login.js
+++ career-ambition/src/components/Login/Login.js
@@ -1,5 +1,42 @@
+import React, { useState,useEffect } from "react";
 import './login.scss'
+import { signInWithEmailAndPassword, onAuthStateChanged  } from "firebase/auth";
+import { useNavigate } from "react-router";
+import { auth} from "../../firebase/firebase-config"
+
+
 export default function Login() {
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            navigate("dashboard");
+          } else {
+            navigate("/");
+          }
+        });
+      }, []);
+
+    const handleLogin = (email, password) => {
+
+        signInWithEmailAndPassword(auth, email, password)
+          .then(() => {
+            //navigate("dashboard");
+            console.log("ya entre")
+            // ...
+          })
+          .catch(() => {
+            setError('Contraseña y/o correo inválidos, vuelve a intentar');
+            setTimeout(() => setError(''), 2500);
+    
+          });
+      };
+
 
     return(
         <div>
@@ -12,11 +49,13 @@ export default function Login() {
                 <p>Name:</p>
                     <input type="text" />
                 <p>Email:</p>
-                    <input type="text" />
+                    <input type="text" onChange={(e) => { setEmail(e.target.value) }} />
                 <p>Password:</p>
-                    <input type="text" />
-                <p><button type="submit">Get in</button> </p>
-            
+                    <input type="password" onChange={(e) => { setPass(e.target.value) }}/>
+                <p><button onClick={() => { handleLogin(email, pass) }}>Get in</button> </p>
+                <div>
+                {error && <p className="error" >{error}</p>}
+                </div>
                 </form>
             </div>
         </div>
--- career-ambition/src/firebase/firebase-config.js
+++ career-ambition/src/firebase/firebase-config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
@@ -12,4 +13,5 @@ const firebaseConfig = {
   appId: "1:568383482690:web:14ddc343d583991d0e76db"
 };
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+ const app = initializeApp(firebaseConfig);
+ export const auth = getAuth();
