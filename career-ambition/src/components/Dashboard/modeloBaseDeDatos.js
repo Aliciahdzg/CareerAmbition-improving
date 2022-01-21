a582f657dbee7d08bbff561b9994c78912dc7d68
Alicia Hernandez <hernndez.alicia4@gmail.com>
2022-01-21 12:57:16 -0600

    agrega estilos dashboard, variables y base de datos modelo

---
+++ career-ambition/src/components/Dashboard/modeloBaseDeDatos.js
@@ -0,0 +1,68 @@
+const data = {
+    "user":{
+      "name":"",
+      "email":"",
+      "careerAmbitions":[
+      {
+        "ambitionName":"",
+        "category":[],
+        "year":2021,
+        "Q1":{
+          "actionPlans":[{"smartGoal":"", "byWhen":""}], 
+          "plannedDeliberate":[{"action":"", "frecuency":""}], 
+          "accountability":{"person":"", "frecuency":"", "vía":""}, 
+          "deliberatePractice":[{"action":"", "date":"", "outcomes":""}]
+        },
+        "Q2":{
+          "actionPlans":[{"smartGoal":"", "byWhen":""}], 
+          "plannedDeliberate":[{"action":"", "frecuency":""}], 
+          "accountability":{"person":"", "frecuency":"", "vía":""}, 
+          "deliberatePractice":[{"action":"", "date":"", "outcomes":""}]
+        },
+        "Q3":{
+          "actionPlans":[{"smartGoal":"", "byWhen":""}], 
+          "plannedDeliberate":[{"action":"", "frecuency":""}], 
+          "accountability":{"person":"", "frecuency":"", "vía":""}, 
+          "deliberatePractice":[{"action":"", "date":"", "outcomes":""}]
+        },
+        "Q4":{
+          "actionPlans":[{"smartGoal":"", "byWhen":""}], 
+          "plannedDeliberate":[{"action":"", "frecuency":""}], 
+          "accountability":{"person":"", "frecuency":"", "vía":""}, 
+          "deliberatePractice":[{"action":"", "date":"", "outcomes":""}]
+        },
+      },
+      {
+        "ambitionName":"",
+        "category":[],
+        "year":2021,
+        "Q1":{
+          "actionPlans":[{"smartGoal":"", "byWhen":""}], 
+          "plannedDeliberate":[{"action":"", "frecuency":""}], 
+          "accountability":{"person":"", "frecuency":"", "vía":""}, 
+          "deliberatePractice":[{"action":"", "date":"", "outcomes":""}]
+        },
+        "Q2":{
+          "actionPlans":[{"smartGoal":"", "byWhen":""}], 
+          "plannedDeliberate":[{"action":"", "frecuency":""}], 
+          "accountability":{"person":"", "frecuency":"", "vía":""}, 
+          "deliberatePractice":[{"action":"", "date":"", "outcomes":""}]
+        },
+        "Q3":{
+          "actionPlans":[{"smartGoal":"", "byWhen":""}], 
+          "plannedDeliberate":[{"action":"", "frecuency":""}], 
+          "accountability":{"person":"", "frecuency":"", "vía":""}, 
+          "deliberatePractice":[{"action":"", "date":"", "outcomes":""}]
+        },
+        "Q4":{
+          "actionPlans":[{"smartGoal":"", "byWhen":""}], 
+          "plannedDeliberate":[{"action":"", "frecuency":""}], 
+          "accountability":{"person":"", "frecuency":"", "vía":""}, 
+          "deliberatePractice":[{"action":"", "date":"", "outcomes":""}]
+        },
+      },
+      ]
+    }
+}
+
+export default data;
