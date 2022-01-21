8566b21e45e293b39aa92b6c92a46796deef7aca
fanita90 <parroquini_90@hotmail.com>
2022-01-21 12:38:29 -0600

    se arregló el reload de login

--- career-ambition/src/App.js
+++ career-ambition/src/App.js
@@ -1,3 +1,6 @@
+import React,{ useEffect, useState}from 'react'
+import { onAuthStateChanged } from '@firebase/auth';
+import { auth } from './firebase/firebase-config';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Login from "./components/Login/Login";
@@ -5,13 +8,27 @@ import Dashboard from "./components/Dashboard/Dashboard"
 import Commitments from "./components/Commitment/Commitments";
 import Calendar from "./components/Calendar/Calendar"
 function App() {
+  const [isLoggedIn, setIsLoggedIn]= useState(false)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true)
+      } else {
+        setIsLoggedIn(false)
+      }
+    });
+  }, []);
+  console.log(isLoggedIn)
+
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/commitment" element={<Commitments />} />
-        <Route path="/calendar" element={<Calendar />} />
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="dashboard" element={<Dashboard />} />
+        <Route exact path="commitment" element={<Commitments />} />
+        <Route exact path="calendar" element={<Calendar />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ career-ambition/src/components/Login/FormLogin.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+// eslint-disable-next-line react/prop-types
+export const FormLogin = ({ handleLogin }) => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPass(e.target.value);
+  return (
+    <>
+      <form className="p-form">
+                  
+                  <p > &#9993;Email:</p>
+                  <input className="input-login" type="text" pattern=".+@improving\.com" size="30" onChange={handleEmail} />
+                  <p>	&#128477; Password:</p>
+                  <input className="input-login" type="password" size="6" pattern="[0-9]{6}"
+        required onChange={handlePassword}/>
+
+                  
+              </form>
+     
+              <button className="login-btn" onClick={() => { handleLogin(email, pass) ;}}>
+                   <span>Get in</span>
+                   <div className="liquid"></div>
+                  </button> 
+    </>
+  );
+};
