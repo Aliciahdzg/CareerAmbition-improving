59448a2f9e7656ef4cdcdddd2195cca28a1a1176
Alicia Hernandez <hernndez.alicia4@gmail.com>
2022-01-22 00:22:07 -0600

    se cambian versiones de dos dependendencias package-lock

--- career-ambition/src/components/Calendar/CalendarView.js
+++ career-ambition/src/components/Calendar/CalendarView.js
@@ -1,14 +1,14 @@
-import 'antd/dist/antd.css';
+//import 'antd/dist/antd.css';
 import './Calendar.scss'
-import { Calendar } from 'antd';
-import {monthCellRender} from './Algo'
+//import { Calendar } from 'antd';
+//import {monthCellRender} from './Algo'
 import Aside from "../Aside/Aside";
 const CalendarView = () =>{
     return(
         <div>
             <Aside />
             <h1>SOY EL CALENDARIO</h1>
-            <Calendar  monthCellRender={monthCellRender} className="cal"/>
+            {/*<Calendar  monthCellRender={monthCellRender} className="cal"/>*/}
         </div>
     )
 }
