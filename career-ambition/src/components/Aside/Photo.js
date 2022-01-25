df4767cab74e7b1b18b6d84a08e6d2a15983e08e
fanita90 <parroquini_90@hotmail.com>
2022-01-24 19:42:49 -0600

    profile image

--- career-ambition/src/components/Aside/Photo.js
+++ career-ambition/src/components/Aside/Photo.js
@@ -10,14 +10,10 @@ const Photo = () => {
     {selectedImage && (
       <div className="img-profile">
       <img alt="not fount" className="profile" src={URL.createObjectURL(selectedImage)} />
-      {/*<br />
-      <button className="btn-delete-photo" onClick={()=>setSelectedImage(null)}>x</button>*/}
-      </div>
-    )}
-  
-   
       <label for="fichero" className="circle">Add photo</label>
       <input type="file" id="fichero" onChange={(event) => {setSelectedImage(event.target.files[0]); }}/>
+      </div>
+    )}
   </div>
   );
 };
